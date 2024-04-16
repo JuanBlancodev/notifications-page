735195bf72533d9bef962ecaa38beefea77e3b84
Juan Blanco <juanblancodev@gmail.com>
2024-04-15 23:40:46 -0700

    wrapper added and axios requests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,23 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  padding: 25px 15px;
+`
+
+const Content = styled.div`
+  height: 100%;
+`
+
 const App = () => {
   return (
-    <div>App</div>
+    <Wrapper className='bg-white'>
+      <Content>
+
+      </Content>
+    </Wrapper>
   )
 }
 
--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,12 +1,61 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from 'prop-types'
+import axios from 'axios'
 
 const GlobalContext = createContext()
 
 const GlobalContextProvider = ({ children }) => {
+  const [users, setUsers] = useState(undefined)
+  const [notifications, setNotifications] = useState(undefined)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try{
+        const { data } = await axios.get('src/db/users.json')
+
+        if(!Array.isArray(data)){
+          throw Error()
+        }
+
+        setUsers(data)
+      }catch(error){
+        setError('An unexpected error occurred while trying to get users.')
+      }
+    }
+
+    fetchUsers()
+  }, [])
+
+  useEffect(() => {
+    const Notifications = async () => {
+      try{
+        const { data } = await axios.get('src/db/notifications.json')
+
+        if(!Array.isArray(data)){
+          throw Error()
+        }
+
+        setNotifications(data)
+      }catch(error){
+        setError('An unexpected error occurred while trying to get notifications.')
+      }
+    }
+
+    Notifications()
+  }), []
+
+  if(error !== null){
+    return <h1>{ error }</h1>
+  }
+  else if(users === undefined || notifications === undefined){
+    return null
+  }
+
   return (
     <GlobalContext.Provider value={{
-
+      users,
+      notifications
     }}>
       { children }
     </GlobalContext.Provider>
