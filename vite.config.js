5cc7d5031f2c654459ac56214f26dfdbb94c0e40
Juan Blanco <juanblancodev@gmail.com>
2024-04-16 14:36:44 -0700

    base added in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,6 @@ export default defineConfig({
         autoprefixer({})
       ]
     }
-  }
+  },
+  base: 'https://juanblancodev.github.io/notifications-page/'
 })
-
-// base: 'https://juanblancodev.github.io/notifications-page/'
